49611adf4659538b414607b8969b1b5b8eff29ea
LRiceBall <65839666+LRiceBall@users.noreply.github.com>
2020-10-10 13:09:29 -0500

    clean data & query

---
+++ Rice/query1.sql
@@ -0,0 +1,16 @@
+create table chicago_df (
+	"name" text,
+	"address" text,
+	"year_installed" int,
+	"artist" text,
+	"landmark_designation_date" text,
+	"latitude" float,
+	"longitude" float,
+	"zip_code" text,
+	"type" text,
+	"description_of_artwork" text
+	);
+	
+select * from chicago_df
+
+
