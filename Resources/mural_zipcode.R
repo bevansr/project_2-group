ef6c02eb976152c05014dd8b47d2c65b3344d8be
Jiaping shen <65934350+choushuiguo325@users.noreply.github.com>
2020-10-16 17:38:21 -0500

    deleted the resorces folder

--- Resources/mural_zipcode.R
+++
@@ -1,6 +0,0 @@
-install.packages("ggmap")
-library("ggmap")
-lonlat_sample <- c(-87.6172043949,41.808529769)
-res <- revgeocode(lonlat_sample,  output = c("address", "all"))
-res$postal_code
-res$neighborhood
