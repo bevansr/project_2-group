b8d2235e0c1e9c23606f8367159f0efbc1a04ced
Daniel Aleksander Karpowicz <A.DanielKarpowicz@gmail.com>
2020-10-17 08:15:47 -0500

    Add files via upload

---
+++ createtableDK.sql
@@ -0,0 +1,14 @@
+CREATE TABLE public.landmarks
+(
+	name text,
+	address text,
+	install_date text,
+	designation_date text,
+	artist text,
+	latitude float,
+	longitude float,
+	zip_code int,
+	type text,
+	description_of_artwork text
+	
+);
